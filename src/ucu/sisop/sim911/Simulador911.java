8cde54feac127e2acb9c7d49c74067a9eb6b274d
Daniel Ramos <dantomramos@gmail.com>
2013-05-19 19:48:07 -0300

    Cambio en Impresion
    Impresion de nombres sin acento

--- src/ucu/sisop/sim911/Simulador911.java
+++ src/ucu/sisop/sim911/Simulador911.java
@@ -4,7 +4,7 @@ public class Simulador911 {
 	public static void main(String[] args) {
 		System.out.println("=== Simulador911 ===");
 		System.out.println("* Daniel Ramos");
-		System.out.println("* Nicolás Expósito");
-		System.out.println("* Andrés Musetti");
+		System.out.println("* Nicolas Exposito");
+		System.out.println("* Andres Musetti");
 	}
 }
