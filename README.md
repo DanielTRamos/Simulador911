661cd675a14e06899e35324ac95fcfdbdb7af646
Andrés Musetti Millán <andresmusetti@gmail.com>
2013-05-19 18:53:51 -0300

    Crear proyecto para Eclipse.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ Simulador911
 ============
 
 Un simple simulador de central de llamadas telefónicas de emergencia.
+
+Integrantes
+===========
+* Daniel Ramos
+* Nicolás Expósito
+* Andrés Musetti
---
+++ src/ucu/sisop/sim911/Simulador911.java
@@ -0,0 +1,10 @@
+package ucu.sisop.sim911;
+
+public class Simulador911 {
+	public static void main(String[] args) {
+		System.out.println("=== Simulador911 ===");
+		System.out.println("* Daniel Ramos");
+		System.out.println("* Nicolás Expósito");
+		System.out.println("* Andrés Musetti");
+	}
+}
